aa867f4da04c55766c799cd7bc80a70508528811
Simon Claessens <gagalago@gmail.com>
2017-12-28 15:40:56 +0100

    add test for transparent scopes

--- test/machine_with_scopes_test.rb
+++ test/machine_with_scopes_test.rb
@@ -19,6 +19,13 @@ class MachineWithScopesTest < BaseTestCase
     assert_equal [parked], @model.with_state(:parked).all
   end
 
+  def test_should_allow_transparent_with_state_in_singular_with_scope
+    @model.create :state => 'parked'
+    @model.create :state => 'idling'
+
+    assert_equal @model.all, @model.with_state(nil).all
+  end
+
   def test_should_create_plural_with_scope
     assert @model.respond_to?(:with_states)
   end
@@ -28,6 +35,13 @@ class MachineWithScopesTest < BaseTestCase
     idling = @model.create :state => 'idling'
 
     assert_equal [parked, idling], @model.with_states(:parked, :idling).all
+  end
+
+  def test_should_allow_transparent_with_states_in_plural_with_scope
+    @model.create :state => 'parked'
+    @model.create :state => 'idling'
+
+    assert_equal @model.all, @model.with_states(nil).all
   end
 
   def test_should_allow_lookup_by_string_name
@@ -43,9 +57,16 @@ class MachineWithScopesTest < BaseTestCase
 
   def test_should_only_include_records_without_state_in_singular_without_scope
     parked = @model.create :state => 'parked'
-    idling = @model.create :state => 'idling'
+    @model.create :state => 'idling'
 
     assert_equal [parked], @model.without_state(:idling).all
+  end
+
+  def test_allow_transparent_without_state_in_singular_without_scope
+    @model.create :state => 'parked'
+    @model.create :state => 'idling'
+
+    assert_equal @model.all, @model.without_state(nil).all
   end
 
   def test_should_create_plural_without_scope
@@ -55,16 +76,30 @@ class MachineWithScopesTest < BaseTestCase
   def test_should_only_include_records_without_states_in_plural_without_scope
     parked = @model.create :state => 'parked'
     idling = @model.create :state => 'idling'
-    first_gear = @model.create :state => 'first_gear'
+    @model.create :state => 'first_gear'
 
     assert_equal [parked, idling], @model.without_states(:first_gear).all
   end
 
+  def test_allow_transparent_without_states_in_plural_without_scope
+    @model.create :state => 'parked'
+    @model.create :state => 'idling'
+    @model.create :state => 'first_gear'
+
+    assert_equal @model.all, @model.without_states(nil).all
+  end
+
   def test_should_allow_chaining_scopes
-    parked = @model.create :state => 'parked'
+    @model.create :state => 'parked'
     idling = @model.create :state => 'idling'
 
     assert_equal [idling], @model.without_state(:parked).with_state(:idling).all
   end
-end
 
+  def test_should_allow_chaining_transparent_scopes
+    @model.create :state => 'parked'
+    idling = @model.create :state => 'idling'
+
+    assert_equal [idling], @model.with_state(nil).with_state(:idling).all
+  end
+end
