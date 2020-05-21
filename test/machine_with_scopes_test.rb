489deeff8b134597c41f22a9d973db63e1f7d129
Jean Boussier <jean.boussier@gmail.com>
2020-05-21 12:13:21 +0200

    Test against Ruby 2.7 and fix warnings

--- test/machine_with_scopes_test.rb
+++ test/machine_with_scopes_test.rb
@@ -43,7 +43,7 @@ class MachineWithScopesTest < BaseTestCase
 
   def test_should_only_include_records_without_state_in_singular_without_scope
     parked = @model.create :state => 'parked'
-    idling = @model.create :state => 'idling'
+    @model.create :state => 'idling'
 
     assert_equal [parked], @model.without_state(:idling).all
   end
@@ -55,13 +55,13 @@ class MachineWithScopesTest < BaseTestCase
   def test_should_only_include_records_without_states_in_plural_without_scope
     parked = @model.create :state => 'parked'
     idling = @model.create :state => 'idling'
-    first_gear = @model.create :state => 'first_gear'
+    @model.create :state => 'first_gear'
 
     assert_equal [parked, idling], @model.without_states(:first_gear).all
   end
 
   def test_should_allow_chaining_scopes
-    parked = @model.create :state => 'parked'
+    @model.create :state => 'parked'
     idling = @model.create :state => 'idling'
 
     assert_equal [idling], @model.without_state(:parked).with_state(:idling).all
