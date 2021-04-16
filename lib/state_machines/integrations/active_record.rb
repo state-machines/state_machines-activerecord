626d5758a0ddf8a737e94b8e1ff3774cc8a4e213
Joel Van Horn <joel@joelvanhorn.com>
2021-04-16 19:39:47 -0400

    Map attribute aliases to column names when initializing

--- lib/state_machines/integrations/active_record.rb
+++ lib/state_machines/integrations/active_record.rb
@@ -445,7 +445,8 @@ module StateMachines
         define_helper :instance, <<-end_eval, __FILE__, __LINE__ + 1
           def initialize(attributes = nil, *)
             super(attributes) do |*args|
-              scoped_attributes = (attributes || {}).merge(self.class.scope_attributes)
+              attributes = (attributes || {}).transform_keys { |key| self.class.attribute_aliases[key.to_s] || key }
+              scoped_attributes = attributes.merge(self.class.scope_attributes)
 
               self.class.state_machines.initialize_states(self, {}, scoped_attributes)
               yield(*args) if block_given?
